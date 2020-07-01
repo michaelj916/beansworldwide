93f00b232198b58ca39e005ab4cb5f5b2cfa6894
michaelj916 <46703736+michaelj916@users.noreply.github.com>
2020-07-01 11:39:03 -0700

    lazyLoad function with IntersectionObserver();

---
+++ bwmedia/lzload.js
@@ -0,0 +1,23 @@
+const targets = document.querySelectorAll('[data-src]');
+
+const lazyLoad = target => {
+    const io = new IntersectionObserver((entries, observer) => {
+
+        entries.forEach(entry => {
+            console.log('working');
+
+            if (entry.isIntersecting) {
+                const img = entry.target;
+                const src = img.getAttribute('data-src');
+
+                img.setAttribute('src', src);
+                
+                observer.disconnect();
+            }
+        });
+    });
+
+    io.observe(target);
+};
+
+targets.forEach(lazyLoad);
